b54186dc581a7fdb4e2bb843215d87ecfcf4d093
cptvictor95 <cpt.victor@hotmail.com>
2021-02-08 18:55:24 -0300

    route functions made, route names changed, buttons and content centralized

---
+++ src/routes/Coordinator.js
@@ -0,0 +1,11 @@
+export const goHome = (history) => {
+  history.push("/");
+};
+
+export const goToPokedex = (history) => {
+  history.push("/pokedex");
+};
+
+export const goToDetails = (history) => {
+  history.push("/details");
+};
